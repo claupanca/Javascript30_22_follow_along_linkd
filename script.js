f57e7ee908ba2072499439d21853f450ede59a58
claupanca <55442866+claupanca@users.noreply.github.com>
2023-12-11 14:01:47 +0200

    Finished

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ console.log(triggers);
 
 // we CREATE THE HIGHLIGHT
 const highlight = document.createElement("span");
+// we add css the properties
+
+// WE can add all these into the CSS STYLE sheet under the .highlight class
 highlight.style.display = "inline-block";
 highlight.style.backgroundColor = "white";
 highlight.style.zIndex = -1;
 highlight.style.position = "absolute";
-// we add the class
+highlight.style.borderRadius = "5px";
+highlight.style.transition = "all 0.2s";
 highlight.classList.add("highlight");
 
 // put it into the DOM
@@ -29,25 +33,25 @@ function higlhlightLink(e) {
   // WE NEED TO FIGURE OUT WHERE ON THE PAGE IS THE ELEMENT
   console.log(this.getBoundingClientRect());
 
+  // get the coords of the hover element
+  const coords = {
+    // we will add the OFFSET in case the user scrolls
+    top: this.getBoundingClientRect().y + window.scrollY,
+    left: this.getBoundingClientRect().x + window.scrollX,
+    width: this.getBoundingClientRect().width,
+    height: this.getBoundingClientRect().height,
+  };
+
   // Set the Transform style to match the Link we hover over
-  let hoverX, hoverY;
-  [hoverX, hoverY] = [
-    this.getBoundingClientRect().x,
-    this.getBoundingClientRect().y,
-  ];
+
   // console.log(hoverX, hoverY);
-  const myY = -initHighlightY + hoverY;
-  highlighted.style.transform = `translate(${hoverX - 3}px, ${myY}px)`;
+  const myY = -initHighlightY + coords.top;
+  highlighted.style.transform = `translate(${coords.left - 3}px, ${myY}px)`;
 
   // Set the height and width of the span to match the Hover over word
-  let hoverWidth, hoverHeight;
-  [hoverWidth, hoverHeight] = [
-    this.getBoundingClientRect().width,
-    this.getBoundingClientRect().height,
-  ];
   // console.log(hoverWidth, hoverHeight);
-  highlighted.style.width = `${hoverWidth + 6}px`;
-  highlighted.style.height = `${hoverHeight}px`;
+  highlighted.style.width = `${coords.width + 6}px`;
+  highlighted.style.height = `${coords.height}px`;
 }
 
 triggers.forEach((item) => item.addEventListener("mouseenter", higlhlightLink));
